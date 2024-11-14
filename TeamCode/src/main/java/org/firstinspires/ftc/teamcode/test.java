2c4506010c74c3a5772596a6aee44f4cb88de718
simon-3000 <simondriley@gmail.com>
2024-11-14 15:35:13 -0500

    Update test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -7,15 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class test extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("hello", "world");
     }
 
     static int counter=0;
     @Override
     public void loop(){
-        if(counter<10) {
-            telemetry.addData("count", "" + counter);
-        }
-        counter++;
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("A button", gamepad1.a);
+
     }
 }
