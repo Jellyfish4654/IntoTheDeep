0356258239d577a511e18b6188cdd40fb815e552
abi <139939572+abibab88@users.noreply.github.com>
2024-11-22 16:48:14 -0500

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,5 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Intake {
-    //placeholder
+public class Intake 
+{
+    private DcMotor armMotor;
+    armMotor = hardwareMap.get(DcMotor.class, "Arm Motor");
+    
+    private boolean initialized = false;
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket)
+
+    if (!initialized) {
+			setTargetPosition(360);
+			initialized = true;
+
+        start() {
+            if (Gamepad1.b.isPressed) {
+                int desiredPosition = 1000
+                armMotor.setTargetPosition (desiredPosition);
+                armmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                }
+        
+            if (!currentGamepad1.b && previousGamepad1.b) {
+                armMotor.setPosition(armMotor.getPosition() - 0.1);
+                }
+
+    
+            }    
+    }
 }
+
