8753478805a9cd1f567e704a4517ea7e51e346a2
ericisaturd <eyiranchen@gmail.com>
2024-11-27 14:43:43 -0500

    today I ate jocelyn's m&ms

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/SlewRateLimiter.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Framework;
+
+import androidx.core.math.MathUtils;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SlewRateLimiter
+{
+    private double maxIncreasePerSecond;
+    private double maxDecreasePerSecond;
+    private final ElapsedTime timer;
+    private double lastValue;
+    private double lastUpdateTime;
+
+    public SlewRateLimiter(double positiveRateLimit, double negativeRateLimit, double initialValue)
+    {
+        this.maxIncreasePerSecond = positiveRateLimit;
+        this.maxDecreasePerSecond = negativeRateLimit;
+        this.lastValue = initialValue;
+        this.lastUpdateTime = 0;
+        this.timer = new ElapsedTime();
+    }
+
+    public SlewRateLimiter(double rateLimit, double initialValue)
+    {
+        this(rateLimit, -rateLimit, initialValue);
+    }
+
+    public SlewRateLimiter(double rateLimit)
+    {
+        this(rateLimit, 0);
+    }
+
+    public void setRate(double rateLimit)
+    {
+        this.maxIncreasePerSecond = rateLimit;
+        this.maxDecreasePerSecond = -rateLimit;
+    }
+
+    public double calculate(double input)
+    {
+        double currentTime = timer.seconds();
+        double elapsedTime = currentTime - lastUpdateTime;
+
+        // Limit the change in value based on the rate limits and elapsed time
+        lastValue +=
+                MathUtils.clamp(
+                        input - lastValue,
+                        maxDecreasePerSecond * elapsedTime,
+                        maxIncreasePerSecond * elapsedTime);
+        lastUpdateTime = currentTime;
+        return lastValue;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JeleTelly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JeleTelly.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import static android.icu.util.UniversalTimeScale.MAX_SCALE;
+
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,15 +12,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.teamcode.Framework.SlewRateLimiter;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.BaseOpMode;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
 @TeleOp(name = "JeleTelly", group = "OpMode")
 public class JeleTelly extends BaseOpMode {
     private final double ENDGAME_ALERT_TIME = 110.0;
+    private final double DEADBAND_VALUE = 0.02;
+    private final double STRAFE_ADJUSTMENT_FACTOR = (14.0/13.0);
+    private double resetHeading = 0;
+    private final SlewRateLimiter[] slewRateLimiters = new SlewRateLimiter[4];
+    private MecanumDrive drive;
     GamepadEx thegamepad;
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
@@ -38,6 +49,8 @@ public class JeleTelly extends BaseOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         initHardware();
+        initializeSlewRateLimiters();
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
         imu = hardwareMap.get(IMU.class, "imu");
 
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
@@ -53,26 +66,7 @@ public class JeleTelly extends BaseOpMode {
             controlSlideMotors();
             controlOuttake();
 
-//            double Yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-//            double y = -gamepad1.left_stick_y;
-//            double x = gamepad1.left_stick_x;
-//            double r = gamepad1.right_stick_x;
-//            double x2 = x*Math.cos(-Yaw)-y*Math.sin(-Yaw);
-//            double y2 = x*Math.sin(-Yaw)+y*Math.cos(-Yaw);
-//            double denominator = Math.max((Math.abs(y2)+Math.abs(x2)+Math.abs(r)),1);
-//            frontLeftMotor.setPower((y2+x2+r)/denominator);
-//            backLeftMotor.setPower((y2-x2+r)/denominator);
-//            frontRightMotor.setPower((y2-x2-r)/denominator);
-//            backRightMotor.setPower((y2+x2-r)/denominator);
-//            if (GamepadEx1.wasJustPressed(GamepadKeys.Button.BACK)) {
-//                imu.resetYaw();
-//            }
-//            double y3 = -gamepad2.right_stick_y;
-//            slideMotorLeft.setPower(y3);
-//            slideMotorRight.setPower(y3);
-//            slideMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
-//
-//            telemetry.update();
+
         }
 
 
@@ -108,4 +102,144 @@ public class JeleTelly extends BaseOpMode {
         }
         //resetIMU();
     }
+    private void updateDriveMode(double precisionMultiplier)
+    {
+        double[] motorSpeeds;
+        switch (driveMode)
+        {
+            case MECANUM:
+                motorSpeeds = MecanumDrive();
+                break;
+            case FIELDCENTRIC:
+                motorSpeeds = FieldCentricDrive();
+                break;
+            case DWFIELDCENTRIC:
+            default:
+              //  motorSpeeds = DWFieldCentricDrive();
+              //  break;
+        }
+        setMotorSpeeds(precisionMultiplier, motorSpeeds);
+    }
+    private double[] MecanumDrive()
+    {
+        double pivot = applyDeadband(gamepad1.right_stick_x);
+        double strafe = applyDeadband(gamepad1.left_stick_x) * STRAFE_ADJUSTMENT_FACTOR;
+        double forward = -applyDeadband(gamepad1.left_stick_y);
+        return new double[]{
+                forward + strafe + pivot,
+                forward - strafe + pivot,
+                forward - strafe - pivot,
+                forward + strafe - pivot
+        };
+    }
+    private double applyDeadband(double joystickValue)
+    {
+        double sign = Math.signum(joystickValue);
+        return joystickValue + (-sign * DEADBAND_VALUE);
+    }
+    private double[] FieldCentricDrive()
+    {
+        double y = -applyDeadband(gamepad1.left_stick_y);
+        double x = applyDeadband(gamepad1.left_stick_x) * STRAFE_ADJUSTMENT_FACTOR;
+        double r = applyDeadband(gamepad1.right_stick_x);
+        double Yaw = imuSensor.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS) - resetHeading;
+
+        double x2 = x * Math.cos(-Yaw) - y * Math.sin(-Yaw);
+        double y2 = x * Math.sin(-Yaw) + y * Math.cos(-Yaw);
+        return new double[]{
+                y2 + x2 + r,
+                y2 - x2 + r,
+                y2 - x2 - r,
+                y2 + x2 - r
+
+        };
+    }
+    protected void setMotorSpeeds(double multiplier, double[] powers)
+    {
+        applyPrecisionAndScale(multiplier, powers);
+        int averagePosition = (slideMotorLeft.getCurrentPosition() + slideMotorRight.getCurrentPosition() / 2);
+        double rate = 1.0;
+        if (averagePosition >= 2000)
+        {
+            rate = 0.99 - ((double) (averagePosition - 2000) / 10) * 0.0005;
+            rate = Math.max(rate, 0);
+            applySlewRateLimit(powers, rate);
+        }
+        for (int i = 0; i < driveMotors.length; i++)
+        {
+            driveMotors[i].setPower(powers[i]);
+        }
+    }
+    private void applyPrecisionAndScale(double multiplier, double[] powers)
+    {
+        for (int i = 0; i < powers.length; i++)
+        {
+            powers[i] *= multiplier;
+        }
+
+        double maxPower = findMaxPower(powers);
+        double scale = maxPower > MAX_SCALE ? MAX_SCALE / maxPower : 1.0;
+
+        for (int i = 0; i < powers.length; i++)
+        {
+            powers[i] *= scale;
+        }
+    }
+    private void applySlewRateLimit(double[] powers, double rate)
+    {
+        for (int i = 0; i < slewRateLimiters.length; i++)
+        {
+            slewRateLimiters[i].setRate(rate);
+            powers[i] = slewRateLimiters[i].calculate(powers[i]);
+        }
+    }
+    private void initializeSlewRateLimiters()
+    {
+        for (int i = 0; i < slewRateLimiters.length; i++)
+        {
+            slewRateLimiters[i] = new SlewRateLimiter(1.0);
+        }
+    }
+    private double findMaxPower(double[] powers)
+    {
+        double max = 0;
+        for (double power : powers)
+        {
+            max = Math.max(max, Math.abs(power));
+        }
+        return max;
+    }
+
+    //  private double[] DWFieldCentricDrive()
+  // {
+     //   double y = -applyDeadband(gamepad1.left_stick_y);
+       // double x = applyDeadband(gamepad1.left_stick_x) * STRAFE_ADJUSTMENT_FACTOR;
+        //double r = applyDeadband(gamepad1.right_stick_x);
+       // double botHeading = drive.pose.heading.toDouble();
+
+        // double x2 = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        // double y2 = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        // return new double[]{
+           //     y2 + x2 + r,
+           //     y2 - x2 + r,
+           //     y2 - x2 - r,
+           //     y2 + x2 - r
+       // };
+   // }
 }
+//            double x2 = x*Math.cos(-Yaw)-y*Math.sin(-Yaw);
+//            double y2 = x*Math.sin(-Yaw)+y*Math.cos(-Yaw);
+//            double denominator = Math.max((Math.abs(y2)+Math.abs(x2)+Math.abs(r)),1);
+//            frontLeftMotor.setPower((y2+x2+r)/denominator);
+//            backLeftMotor.setPower((y2-x2+r)/denominator);
+//            frontRightMotor.setPower((y2-x2-r)/denominator);
+//            backRightMotor.setPower((y2+x2-r)/denominator);
+//            if (GamepadEx1.wasJustPressed(GamepadKeys.Button.BACK)) {
+//                imu.resetYaw();
+//            }
+//            double y3 = -gamepad2.right_stick_y;
+//            slideMotorLeft.setPower(y3);
+//            slideMotorRight.setPower(y3);
+//            slideMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//            telemetry.update();
