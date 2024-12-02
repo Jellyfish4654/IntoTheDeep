1da60d43062d588c6c15a2365450a0ac9fb38295
Happired <jocelynzheng99@gmail.com>
2024-12-02 16:41:05 -0500

    hjkjhhjkjhk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/doubleServoTuner.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name = "Double Servo Test", group = "Test")
+public class doubleServoTuner extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        final Servo outtakeLeftServo;
+        final Servo outtakeRightServo;
+        outtakeLeftServo = hardwareMap.get(Servo.class, "outtakeArmServoLeft");
+        outtakeRightServo = hardwareMap.get(Servo.class, "outtakeArmServoRight");
+
+//        outtakeLeftServo.setDirection(Servo.Direction.REVERSE);
+        double position = 0; // Initialize to midpoint
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            telemetry.addData("ServoR", position);
+            telemetry.addData("position", outtakeLeftServo.getPosition());
+            telemetry.update();
+
+            outtakeLeftServo.setPosition(position);
+
+            if (gamepad1.dpad_left)
+            {
+                position -= 0.01;
+            }
+            if (gamepad1.dpad_right)
+            {
+                position += 0.01;
+            }
+
+            if (gamepad1.a)
+            {
+                position = 0.50;
+            }
+            else if (gamepad1.b)
+            {
+                position = 0.8;
+            }
+        }
+    }
+}
