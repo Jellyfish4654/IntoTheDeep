1c70490822da18190619d28a53a99ed42320508e
ericisaturd <eyiranchen@gmail.com>
2024-12-05 17:02:03 -0500

    pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Slides.java
@@ -17,10 +17,10 @@ public class Slides {
     private double kPRight = 0.05, kIRight = 0, kDRight = 0.0002;
     //	private double kPLeft = 0.01, kILeft = 0, kDLeft = 0.0002, kFLeft = 0.01;
 //	private double kPRight = 0.01, kIRight = 0, kDRight = 0.0002, kFRight = 0.01;
-    private double lastLeftError = 0, lastRightError = 0;
-    private double leftIntegralSum = 0, rightIntegralSum = 0;
-    private double kG = 1.5;
-    private final double ticks_in_degrees = 537.7 / 360.0;
+    private PIDController controller;
+    public static double f = 0.1;
+    public static int target = -177;
+    private final double ticks_in_degree = 587.3/360;
     private int targetPosition;
     private double voltageCompensation;
 
@@ -54,27 +54,23 @@ public class Slides {
         slideMotorRight.setPower(rightPower);
     }
 
-    private double calculateMotorPowerLeft(DcMotorEx motor, int targetPosition) {
+    private double calculateMotorPowerLeft(DcMotorEx motor, double targetPosition) {
         timer.reset();
-        int currentPosition = motor.getCurrentPosition();
-        int error = targetPosition - currentPosition;
-        double derivative = (error - lastLeftError) / timer.seconds();
-        double integralSum = leftIntegralSum + (error * timer.seconds());
-        double out = (kPLeft * error) + (kILeft * integralSum) + (kDLeft * derivative) * kG;
-        leftIntegralSum = integralSum;
-        lastLeftError = error;
-        return out;
+        controller = new PIDController(kPLeft, kILeft, kDLeft);
+        int leftPos = slideMotorLeft.getCurrentPosition();
+        double pid = controller.calculate(leftPos, target);
+        double ff = Math.cos(Math.toRadians(target/ticks_in_degree)) * f;
+        double power = pid + ff;
+        return power;
     }
     private double calculateMotorPowerRight(DcMotorEx motor, int targetPosition) {
         timer.reset();
-        int currentPosition = motor.getCurrentPosition();
-        int error = targetPosition - currentPosition;
-        double derivative = (error - lastRightError) / timer.seconds();
-        double integralSum = rightIntegralSum + (error * timer.seconds());
-        double out = (kPRight * error) + (kIRight * integralSum) + (kDRight * derivative) * kG;
-        lastRightError = error;
-        rightIntegralSum = integralSum;
-        return out;
+        controller = new PIDController(kPRight, kIRight, kDRight);
+        int rightPos = slideMotorLeft.getCurrentPosition();
+        double pid = controller.calculate(rightPos, target);
+        double ff = Math.cos(Math.toRadians(target/ticks_in_degree)) * f;
+        double power = pid + ff;
+        return power;
     }
     public int getTargetPosition()
     {
