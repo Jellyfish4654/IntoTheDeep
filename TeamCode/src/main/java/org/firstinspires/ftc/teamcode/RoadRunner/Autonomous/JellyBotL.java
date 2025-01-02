78c06c348b82a8f87703000d99864c8f342575d4
ericisaturd <eyiranchen@gmail.com>
2025-01-02 15:32:37 -0500

    MORE JELLYBOT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/Autonomous/JellyBotL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/Autonomous/JellyBotL.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.RoadRunner.Autonomous;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.SequentialAction;
+@Config
+@Autonomous(name = "JellyBotL", group = "Autonomous")
 public class JellyBotL {
+
 }
