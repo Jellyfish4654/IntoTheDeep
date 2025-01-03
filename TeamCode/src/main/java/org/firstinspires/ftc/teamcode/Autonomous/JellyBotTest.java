9fdd509b7eb5d7a806b17ef5a48f03e8f4083d6c
ericisaturd <eyiranchen@gmail.com>
2025-01-03 12:07:57 -0500

    Update JellyBotTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotTest.java
@@ -27,7 +27,7 @@ import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
 @Config
 @Autonomous(name = "TestAuto", group = "Autonomous")
 public class JellyBotTest extends BaseOpMode {
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         // instantiate your MecanumDrive at a particular pose.
         Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
