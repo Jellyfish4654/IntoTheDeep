6b1e23712bea8f36e4a444e5d2e5ed83fba25343
ericisaturd <eyiranchen@gmail.com>
2025-01-09 08:24:42 -0500

    Update JellyBotL.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotL.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,7 +16,62 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.SequentialAction;
+
+import org.firstinspires.ftc.teamcode.Framework.BaseOpMode;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+@Config
 @Autonomous(name = "JellyBotL", group = "Autonomous")
-public class JellyBotL {
-    //eerr
+public class JellyBotL extends BaseOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // instantiate your MecanumDrive at a particular pose.
+        Pose2d initialPose = new Pose2d(23.5, 62, Math.toRadians(270));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Actions.runBlocking(slides.slidesDown());
+
+        TrajectoryActionBuilder toSubmersible = drive.actionBuilder(initialPose)
+                .lineToY(42)
+                .setTangent(Math.toRadians(180))
+                .lineToX(5)
+                .setTangent(Math.toRadians(90))
+                .lineToY(35.3);
+
+        Pose2d secondPose = new Pose2d(5, 35.3, Math.toRadians(90));
+        TrajectoryActionBuilder toSample = drive.actionBuilder(secondPose)
+                .waitSeconds(3) // hang specimen
+                .lineToY(45.3)
+                .setTangent(Math.toRadians(0))
+                .lineToX(48.2)
+                .setTangent(Math.toRadians(270));
+
+        Pose2d thirdPose = new Pose2d(48.2, 45.3, Math.toRadians(270));
+        TrajectoryActionBuilder toBasket = drive.actionBuilder(thirdPose)
+                .waitSeconds(2) // grab sample
+                .lineToY(53.2)
+                .setTangent(Math.toRadians(180))
+                .lineToX(51.8)
+                .setTangent(Math.toRadians(225));
+
+        Pose2d fourthPose = new Pose2d(51.8, 53.2, Math.toRadians(225));
+        TrajectoryActionBuilder toSecondSample = drive.actionBuilder(fourthPose)
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(180))
+                .lineToX(48.2)
+                .setTangent(Math.toRadians(270))
+                .lineToY(45.3)
+                .setTangent(Math.toRadians(0))
+                .lineToX(58.2)
+                .setTangent(Math.toRadians(270));
+
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+
+                )
+        );
+    }
 }
