03b018fdb9d7c205a8d7fb758c9c4f49a0e7a92b
ericisaturd <eyiranchen@gmail.com>
2025-01-17 13:38:20 -0500

    wrist tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/wristTuner.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Tuners;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp(name = "Single Servo Test", group = "Test")
+public class wristTuner extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        final Servo wristServo;
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+
+//        outtakeLeftServo.setDirection(Servo.Direction.REVERSE);
+        double position = wristServo.getPosition();
+        // Initialize to midpoint
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+
+            telemetry.addData("target position", position);
+            telemetry.addData("position", wristServo.getPosition());
+            telemetry.update();
+
+            wristServo.setPosition(position);
+
+            if (gamepad1.dpad_left)
+            {
+                position -= 0.01;
+            }
+            if (gamepad1.dpad_right)
+            {
+                position += 0.01;
+            }
+
+            if (gamepad1.a)
+            {
+                position = 0.5;
+            }
+            else if (gamepad1.b)
+            {
+                position = 0.79;
+            }
+        }
+    }
+}
