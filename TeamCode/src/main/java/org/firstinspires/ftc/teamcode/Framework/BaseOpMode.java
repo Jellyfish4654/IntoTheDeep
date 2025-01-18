10e13d717df3e572b7ef64323b4d0a4a3066fb11
ericisaturd <eyiranchen@gmail.com>
2025-01-18 11:46:59 -0500

    Update BaseOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/BaseOpMode.java
@@ -68,7 +68,6 @@ public abstract class BaseOpMode extends LinearOpMode {
 
         slideMotorLeft = hardwareMap.get(DcMotorEx.class, "slideMotorLeft");
         slideMotorRight = hardwareMap.get(DcMotorEx.class, "slideMotorRight");
-        slideMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         slides = new Slides(slideMotorLeft, slideMotorRight, voltageSensor);
 
