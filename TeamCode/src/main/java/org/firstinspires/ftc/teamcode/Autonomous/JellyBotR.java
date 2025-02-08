0f3456cfd4f4c695e8475c772de6b898cf0ea0a0
ericisaturd <eyiranchen@gmail.com>
2025-02-07 21:43:06 -0500

    autonomous anonymous i throw my mouse at the wall because it no work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JellyBotR.java
@@ -36,74 +36,73 @@ public class JellyBotR extends BaseOpMode {
         Actions.runBlocking(wrist.new WristDown());
 
         waitForStart();
+        if (isStopRequested()) return;
 
-        while (opModeIsActive()) {
-            Actions.runBlocking(
-                    new SequentialAction(
-                            outtakeRotatingArm.new OuttakeDeposit(),
-                            drive.actionBuilder(drive.pose)
-                                    .splineToConstantHeading(new Vector2d(-5, 35.3), Math.toRadians(0))
-                                    .build(),
-                            slides.new SlidesUp(),
-                            outtakeServo.new ClawOpen(),
-                            slides.new SlidesDown()
-                    )
-            );
-            drive.updatePoseEstimate();
-            Actions.runBlocking(
-                    new SequentialAction(
-                            drive.actionBuilder(drive.pose)
-                                    .splineToLinearHeading(new Pose2d(-40.2, 50, Math.toRadians(270)), Math.toRadians(0))
-                                    .splineToConstantHeading(new Vector2d(-48.2, 45.3), Math.toRadians(0))
-                                    .build(),
-                            intakeServo.new ClawOpen(),
-                            extendo.new ExtendoExtend(),
-                            intakeServo.new ClawClose(),
-                            extendo.new ExtendoRetract(),
-                            slides.new SlidesTransfer(),
-                            outtakeRotatingArm.new OuttakeTransfer(),
-                            wrist.new WristUp(),
-                            outtakeServo.new ClawClose(),
-                            intakeServo.new ClawOpen(),
-                            wrist.new WristDown(),
-                            outtakeRotatingArm.new OuttakeDeposit()
-                    )
-            );
-            drive.updatePoseEstimate();
-            Actions.runBlocking(
-                    new SequentialAction(
-                            drive.actionBuilder(drive.pose)
-                                    .splineToConstantHeading(new Vector2d(-48.2, 55.3), Math.toRadians(0))
-                                    .build(),
-                            outtakeServo.new ClawOpen()
-                    )
-            );
-            drive.updatePoseEstimate();
-            Actions.runBlocking(
-                    new SequentialAction(
-                            drive.actionBuilder(drive.pose)
-                                    .splineToConstantHeading(new Vector2d (-58.2, 45.3), Math.toRadians(0))
-                                    .build(),
-                            extendo.new ExtendoExtend(),
-                            intakeServo.new ClawClose(),
-                            extendo.new ExtendoRetract(),
-                            slides.new SlidesTransfer(),
-                            outtakeRotatingArm.new OuttakeTransfer(),
-                            wrist.new WristUp(),
-                            outtakeServo.new ClawClose(),
-                            intakeServo.new ClawOpen(),
-                            wrist.new WristDown()
-                    )
-            );
-            drive.updatePoseEstimate();
-            Actions.runBlocking(
-                    new SequentialAction(
-                            drive.actionBuilder(drive.pose)
-                                    .splineToConstantHeading(new Vector2d (-58.2, 61.3), Math.toRadians(0))
-                                    .build()
-                    )
-            );
-        }
+        Actions.runBlocking(
+                new SequentialAction(
+                        outtakeRotatingArm.new OuttakeDeposit(),
+                        drive.actionBuilder(drive.pose)
+                                .splineToConstantHeading(new Vector2d(-5, 35.3), Math.toRadians(0))
+                                .build(),
+                        slides.new SlidesUp(),
+                        outtakeServo.new ClawOpen(),
+                        slides.new SlidesDown()
+                )
+        );
+        drive.updatePoseEstimate();
+        Actions.runBlocking(
+                new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .splineToLinearHeading(new Pose2d(-40.2, 50, Math.toRadians(270)), Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(-48.2, 45.3), Math.toRadians(0))
+                                .build(),
+                        intakeServo.new ClawOpen(),
+                        extendo.new ExtendoExtend(),
+                        intakeServo.new ClawClose(),
+                        extendo.new ExtendoRetract(),
+                        slides.new SlidesTransfer(),
+                        outtakeRotatingArm.new OuttakeTransfer(),
+                        wrist.new WristUp(),
+                        outtakeServo.new ClawClose(),
+                        intakeServo.new ClawOpen(),
+                        wrist.new WristDown(),
+                        outtakeRotatingArm.new OuttakeDeposit()
+                )
+        );
+        drive.updatePoseEstimate();
+        Actions.runBlocking(
+                new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .splineToConstantHeading(new Vector2d(-48.2, 55.3), Math.toRadians(0))
+                                .build(),
+                        outtakeServo.new ClawOpen()
+                )
+        );
+        drive.updatePoseEstimate();
+        Actions.runBlocking(
+                new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .splineToConstantHeading(new Vector2d (-58.2, 45.3), Math.toRadians(0))
+                                .build(),
+                        extendo.new ExtendoExtend(),
+                        intakeServo.new ClawClose(),
+                        extendo.new ExtendoRetract(),
+                        slides.new SlidesTransfer(),
+                        outtakeRotatingArm.new OuttakeTransfer(),
+                        wrist.new WristUp(),
+                        outtakeServo.new ClawClose(),
+                        intakeServo.new ClawOpen(),
+                        wrist.new WristDown()
+                )
+        );
+        drive.updatePoseEstimate();
+        Actions.runBlocking(
+                new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .splineToConstantHeading(new Vector2d (-58.2, 61.3), Math.toRadians(0))
+                                .build()
+                )
+        );
 
     }
 }
